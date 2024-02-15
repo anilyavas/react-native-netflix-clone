dfd903c9144572f476c0992086bb110c5740c0a1
Anıl <anil@Anl-MacBook-Air.local>
2024-02-15 14:34:02 +0300

    homescreen ui

--- app/(tabs)/Home.tsx
+++ app/(tabs)/Home.tsx
@@ -1,22 +1,13 @@
-import { FlatList, StyleSheet, SafeAreaView, Text, Image } from 'react-native';
+import { StyleSheet, SafeAreaView, FlatList } from 'react-native';
+import HomeCategory from '@/components/HomeCategory';
 import categories from '@/assets/data/categories';
 
-const firstCategory = categories.items[0];
 export default function HomeScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>{firstCategory.title}</Text>
       <FlatList
-        data={firstCategory.movies}
-        renderItem={({ item }) => (
-          <Image
-            style={styles.image}
-            source={{
-              uri: item.poster,
-            }}
-          />
-        )}
-        horizontal
+        data={categories.items}
+        renderItem={({ item }) => <HomeCategory category={item} />}
       />
     </SafeAreaView>
   );
@@ -26,19 +17,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'black',
-    padding: 20,
-  },
-  title: {
-    color: 'white',
-    fontSize: 20,
-    fontWeight: 'bold',
-    margin: 5,
-  },
-  image: {
-    width: 100,
-    height: 170,
-    resizeMode: 'cover',
-    borderRadius: 5,
-    margin: 5,
   },
 });
---
+++ components/HomeCategory.tsx
@@ -0,0 +1,46 @@
+import { Text, FlatList, StyleSheet, Image, View } from 'react-native';
+
+type HomeCategoryProps = {
+  category: {
+    id: string;
+    title: string;
+    movies: { id: string; poster: string };
+  };
+};
+const HomeCategory = ({ category }: HomeCategoryProps) => {
+  return (
+    <View>
+      <Text style={styles.title}>{category.title}</Text>
+      <FlatList
+        data={category.movies}
+        renderItem={({ item }) => (
+          <Image
+            style={styles.image}
+            source={{
+              uri: item.poster,
+            }}
+          />
+        )}
+        horizontal
+      />
+    </View>
+  );
+};
+
+export default HomeCategory;
+
+const styles = StyleSheet.create({
+  title: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 5,
+  },
+  image: {
+    width: 100,
+    height: 170,
+    resizeMode: 'cover',
+    borderRadius: 5,
+    margin: 5,
+  },
+});
